f64b3e2cae33a4503393fee5c8dc7d28f62d0048
jcanaviri42 <jcanaviri42@gmail.com>
2023-07-05 22:23:57 -0400

    Adding email send functionality

--- hello.py
+++ hello.py
@@ -8,7 +8,9 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_mail import Mail, Message
 
+from threading import Thread
 from dotenv import load_dotenv
 
 
@@ -23,10 +25,21 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "hard to guess string"
 
+app.config["MAIL_SERVER"] = "smtp.googlemail.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
+
+app.config["FLASKY_MAIL_SUBJECT_PREFIX"] = "[Flasky]"
+app.config["FLASKY_MAIL_SENDER"] = "Flasky Admin <Flasky@example.com>"
+app.config["FLASKY_ADMIN"] = os.environ.get("FLASKY_ADMIN")
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+mail = Mail(app)
 
 
 class Role(db.Model):
@@ -54,6 +67,24 @@ class NameForm(FlaskForm):
     submit = SubmitField("Submit")
 
 
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message(
+        app.config["FLASKY_MAIL_SUBJECT_PREFIX"] + subject,
+        sender=app.config["FLASKY_MAIL_SENDER"],
+        recipients=[to],
+    )
+    msg.body = render_template(template + ".txt", **kwargs)
+    msg.html = render_template(template + ".html", **kwargs)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
+
+
 @app.get("/")
 @app.post("/")
 def index():
@@ -66,6 +97,11 @@ def index():
             db.session.add(user)
             db.session.commit()
             session["known"] = False
+
+            if app.config["FLASKY_ADMIN"]:
+                send_email(
+                    app.config["FLASKY_ADMIN"], "New User", "mail/new_user", user=user
+                )
         else:
             session["known"] = True
 
