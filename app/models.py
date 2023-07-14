2c3bb528556906d944380d4c349a9f3d9c634934
jcanaviri42 <jcanaviri42@gmail.com>
2023-07-13 21:53:02 -0400

    add flask_login library

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
+from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-from . import db
+
+from . import db, login_manager
 
 
 class Role(db.Model):
@@ -12,12 +14,13 @@ class Role(db.Model):
         return f"<Role {self.name}>"
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__: str = "users"
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.ForeignKey("roles.id"))
     password_hash = db.Column(db.String(128))
+    role_id = db.Column(db.Integer, db.ForeignKey("roles.id"))
 
     @property
     def password(self):
@@ -29,6 +32,10 @@ class User(db.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
     def __repr__(self):
         return f"<User {self.name}>"
